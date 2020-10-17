9f1a4a1cc76752ce1c063a9639a2a61998699b1f
rosalg <rosalg@stanford.edu>
2020-10-17 08:19:46 -0700

    Updated gitignore to exclude .idea

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -25,7 +25,6 @@ def CalculateLogRegGrad(theta, features, y, n):
     return (y - g(theta, features)) * features / n
 
 def LearnPredictor(x_points, y_points, FE, numIters, eta, eps):
-    prev_thetas = []
     thetas = np.zeros((TOTAL_RGB//RGB_GRANULARITY)**3)
     for t in range(numIters):
         for i in range(len(x_points)):
@@ -36,7 +35,6 @@ def LearnPredictor(x_points, y_points, FE, numIters, eta, eps):
             if np.linalg.norm(thetas - prev_thetas) < eps:
                 return thetas
     return thetas
-
 
 def GenerateRandomData(numPoints, imageWidth, imageHeight):
     x_points = tor.randint(TOTAL_RGB, (numPoints, imageWidth * imageHeight, 3))
