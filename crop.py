795b63aee743a52e5dce4a527bb7cb33766626e5
dastratakos <dastratakos@gmail.com>
2020-10-20 12:38:12 -0700

    Add crop.py to create new dataset
    The new dataset is formed by taking each of the original images and
    cropping out each face as specified by the annotations. To create this
    new dataset, you must have the original dataset in ./archive/images and
    ./archive/annotations. Then, run python crop.py, and a new directory will be
    created called ./archive/cropped_images. Furthermore, a file called
    cropped_labels.csv will be created to store the new labels for the
    images matching to their ids.

---
+++ crop.py
@@ -0,0 +1,84 @@
+"""
+file: crop.py
+-------------
+Takes each labeled input data set and creates new data. A new square image is
+created for each person in the image. The corresponding labels (mask, no mask,
+incorrect) are stored in a CSV file.
+
+It should take just over 30 seconds to process 853 images.
+"""
+import csv
+from PIL import Image
+
+from tqdm import tqdm
+
+import preprocess
+
+ARCHIVE_ROOT = './archive/'
+IMAGE_ROOT = f'{ARCHIVE_ROOT}images/'
+CROPPED_IMAGE_ROOT = f'{ARCHIVE_ROOT}cropped_images/'
+CSV_FILE = f'{ARCHIVE_ROOT}cropped_labels.csv'
+SCALE = 1.5
+DIM = 64
+NEW_LABELS = {
+    'without_mask': 'no mask',
+    'with_mask': 'mask',
+    'mask_weared_incorrect': 'incorrect'
+    }
+
+def compute_crop_box(bounding_box):
+    """Computes the coordinates to crop an image based on the provided
+    bounding_box and the SCALE variable. The input only describes a box around
+    the mask (or area where the mask would be), and the width and height could
+    could be different. The output box will be a square and contain a larger
+    portion of the face.
+
+    Args:
+        bounding_box (Dictionary): Describes the original shape of the bounding
+            box.
+    """
+    x_max, x_min, y_max, y_min = [int(v)
+                                    for _, v in sorted(bounding_box.items())]
+
+    # compute half of the side of the scaled box to crop    
+    width = x_max - x_min
+    height = y_max - y_min
+    half_side = SCALE / 2 * max(width, height)
+
+    x_center = (x_max + x_min) / 2
+    y_center = (y_max + y_min) / 2
+    
+    left, top, right, bottom = (int(x_center - half_side),
+                                int(y_center - half_side),
+                                int(x_center + half_side),
+                                int(y_center + half_side))
+
+    return((left, top, right, bottom))
+
+def get_label(label):
+    if label in NEW_LABELS:
+        return NEW_LABELS[label]
+    else:
+        raise ValueError(f"Unknown label category: {label}")
+
+def main():
+    image_bases, annotations = preprocess.main()
+    id = 0
+
+    with open(CSV_FILE, 'w') as f:
+        csv_file = csv.writer(f)
+        csv_file.writerow(['id', 'label'])
+        print(f'Finding faces in {len(image_bases)} images')
+        with tqdm(total=len(image_bases)) as progress_bar:
+            for image_base, annotation in zip(image_bases, annotations):
+                im = Image.open(f'{IMAGE_ROOT}{image_base}')
+                for object in annotation['objects']:
+                    crop_box = compute_crop_box(object['bndbox'])
+                    cropped_image = im.crop(crop_box).resize((DIM, DIM))
+                    cropped_image.save(f'{CROPPED_IMAGE_ROOT}image{id}.png')
+                    csv_file.writerow([id, get_label(object['name'])])
+                    id += 1
+                progress_bar.update()
+
+if __name__ == '__main__':
+    main()
