316a3d80b1256d3995673cc43a572472405ddceb
dastratakos <dastratakos@gmail.com>
2020-10-20 12:35:18 -0700

    Build a description for the command line interface

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,21 +1,39 @@
 import argparse
 from argparse import RawTextHelpFormatter
-# import torch
-# from torchvision import transforms, datasets, models
 
 import preprocess
-# from FaceMaskDataset import FaceMaskDataset
+
+def build_description():
+    lines = [
+        'Face Mask Detection Pipeline',
+        '',
+        'Charles Pan, Gilbert Rosal, & Dean Stratakos',
+        'CS 229: Machine Learning',
+        'October 20, 2020'
+    ]
+    max_length = max(len(l) for l in lines)
+    bar = f"+{'-' * (max_length + 2)}+"
+    description = f'{bar}\n'
+    for line in lines:
+        total_padding = max_length - len(line)
+        left_padding = int(total_padding / 2)
+        right_padding = int((total_padding + 1) / 2)
+        description += f"| {' ' * left_padding}{line}{' ' * right_padding} |\n"
+    description += f'{bar}'
+    return description
 
 def parse_args():
     """ Parse the input arguments """
-    arg_parser = argparse.ArgumentParser(
-        description="+------------------------------+\n| Face Mask Detection Pipeline |\n+------------------------------+",
+    arg_parser = argparse.ArgumentParser(description=build_description(),
         formatter_class=RawTextHelpFormatter)
-    arg_parser.add_argument("model", help="1 for logistic regression, 2 for SVM, 3 for CNN",
-        type=int, choices=[1, 2, 3], default=3)
+    arg_parser.add_argument("model",
+        help="1 for logistic regression, 2 for SVM, 3 for CNN", type=int,
+        choices=[1, 2, 3], default=3)
     group = arg_parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-    group.add_argument("-q", "--quiet", help="decrease output verbosity", action="store_true")
+    group.add_argument("-v", "--verbose", help="increase output verbosity",
+        action="store_true")
+    group.add_argument("-q", "--quiet", help="decrease output verbosity",
+        action="store_true")
 
     args = arg_parser.parse_args()
 
@@ -34,16 +52,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-
     preprocess.main()
-
-    # transform = transforms.Compose([transforms.ToTensor()])
-    # data = FaceMaskDataset(transform)
-    # data_loader = torch.utils.data.DataLoader(
-    #     data,
-    #     batch_size=4,
-    #     collate_fn=lambda batch: tuple(zip(*batch))
-    # )
 
 if __name__ == '__main__':
     main()
