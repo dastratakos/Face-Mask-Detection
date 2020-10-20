df3368e504b0fff435e925caae47ba9b252710dd
Charles Pan <cpan22@stanford.edu>
2020-10-19 23:38:42 -0400

    Implemented score for sklearn SVM

--- svm.py
+++ svm.py
@@ -5,11 +5,12 @@ from sklearn.svm import SVC
 X_train = [[0, 0, 0], [1, 1, 1]]
 Y_train = [0, 1]
 
-X_test = [[-1, -1, -1]]
-Y_test = [1]
+X_val = [[-1, -1, -1]]
+Y_val = [1]
 
 linearclf = make_pipeline(StandardScaler(), SVC())
 linearclf.fit(X_train, Y_train)
 
-predictions = linearclf.predict(X_test)
+predictions = linearclf.predict(X_val)
 print(predictions)
+print("Score is", linearclf.score(X_val, Y_val))
