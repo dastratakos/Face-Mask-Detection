0c49a42c5683d6162c0e44075a9dd01717e02065
Charles Pan <cpan22@stanford.edu>
2020-11-13 01:26:56 -0500

    Added rest of layers on top of ResNet50

--- models/ResNet.py
+++ models/ResNet.py
@@ -5,25 +5,37 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-# data_augmentation = keras.Sequential(
-#     [
-#         layers.experimental.preprocessing.RandomFlip("horizontal"),
-#         layers.experimental.preprocessing.RandomRotation(0.1),
-#     ]
-# )
+data_augmentation = keras.Sequential(
+    [
+        tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+        tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
+    ]
+)
 
-IMG_HEIGHT = 64
-IMG_WIDTH = 64
-IMG_DEPTH = 64
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+IMG_DEPTH = 3
 
 base_model = tf.keras.applications.ResNet50(
     include_top=False,  # don't include fully-connected layer on top so we can build and train our own
     weights="imagenet",
-    input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH)
+    input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH),
 )
 
 # Freeze base_model
 base_model.trainable = False
 
 inputs = keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH))
-# x = data_augmentation(inputs)
+x = data_augmentation(inputs)  # optional data augmentation
+
+x = tf.keras.applications.resnet.preprocess_input(x)  # ResNet50 input preprocessing
+
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)
+x = keras.layers.Dense(3)(x)
+outputs = keras.layers.Activation('softmax')(x)
+
+model = keras.Model(inputs, outputs)
+
+print(model.summary())
