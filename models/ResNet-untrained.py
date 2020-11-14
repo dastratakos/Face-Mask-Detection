59405890b987ce35b927701bfd90fc09efa3b804
Charles Pan <cpan22@stanford.edu>
2020-11-13 23:56:01 -0500

    Fixed untrained ResNet50 to accept variable sized input_shape

--- models/ResNet-untrained.py
+++ models/ResNet-untrained.py
@@ -16,11 +16,12 @@ IMG_HEIGHT = 64
 IMG_WIDTH = 64
 
 inputs = keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3))
+random_uniform_tensor = tf.keras.backend.random_uniform(shape=(IMG_HEIGHT, IMG_WIDTH, 3), minval=0.0, maxval=1.0)
 
 base_model = tf.keras.applications.ResNet50(
-    include_top=True,
+    include_top=False,
     weights=None,
-    classes=3
+    input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
 )
 
 # Don't freeze base_model
@@ -29,7 +30,12 @@ base_model.trainable = True
 # x = data_augmentation(inputs)  # optional data augmentation
 x = inputs
 x = tf.keras.applications.resnet.preprocess_input(x)  # ResNet50 input preprocessing
-outputs = base_model(x, training=False)
+x = base_model(x, training=True)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)
+x = keras.layers.Dense(3)(x)
+outputs = keras.layers.Activation('softmax')(x)
 model = keras.Model(inputs, outputs)
 
 print(model.summary())
+print(model.predict(np.array([random_uniform_tensor])))
