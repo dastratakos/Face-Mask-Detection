5a60aa0fa9d5eb5489b47ffa20683784f942b5dd
Charles Pan <cpan22@stanford.edu>
2020-11-14 15:59:40 -0500

    Added retrieving train and test set to ResNet code

--- models/ResNet-untrained.py
+++ models/ResNet-untrained.py
@@ -4,6 +4,13 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from keras_load_dataset import loadDataset, splitGroups
+
+dataset_directory = ""
+train_split = 0.9
+
+face_mask_dataset = loadDataset(dataset_directory)
+train_set, test_set = splitGroups(face_mask_dataset, train_split)
 
 data_augmentation = keras.Sequential(
     [
@@ -12,8 +19,11 @@ data_augmentation = keras.Sequential(
     ]
 )
 
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
+IMG_HEIGHT = 64
+IMG_WIDTH = 64
+
+inputs = keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3))
+# random_uniform_tensor = tf.keras.backend.random_uniform(shape=(IMG_HEIGHT, IMG_WIDTH, 3), minval=0.0, maxval=1.0)
 
 base_model = tf.keras.applications.ResNet50(
     include_top=False,  # don't include fully-connected layer on top so we can build and train our own
@@ -39,3 +49,4 @@ outputs = keras.layers.Activation('softmax')(x)
 model = keras.Model(inputs, outputs)
 
 print(model.summary())
+# print(model.predict(np.array([random_uniform_tensor])))
