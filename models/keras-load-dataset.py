ee768d0a09031af7ded839e4b40a7807ba4c313b
Charles Pan <cpan22@stanford.edu>
2020-11-14 15:07:25 -0500

    Added code for loading dataset and splitting into train/val/test

---
+++ models/keras-load-dataset.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+def loadDataset(img_directory):
+    face_mask_dataset = tf.keras.preprocessing.image_dataset_from_directory(img_directory, labels='inferred')
+    return face_mask_dataset
+
+def splitGroups(dataset):
+    dataset_size = tf.data.experimental.cardinality(dataset)
+
+    train_size = int(0.8 * dataset_size)
+    val_size = int(0.1 * dataset_size)
+    test_size = int(0.1 * dataset_size)
+
+    dataset = dataset.shuffle()
+    train_dataset = dataset.take(train_size)
+    test_dataset = dataset.skip(train_size)
+    val_dataset = test_dataset.skip(val_size)
+    test_dataset = test_dataset.take(test_size)
+
+    print("Number of training samples: %d" % tf.data.experimental.cardinality(train_dataset))
+    print("Number of validation samples: %d" % tf.data.experimental.cardinality(val_dataset))
+    print("Number of test samples: %d" % tf.data.experimental.cardinality(test_dataset))
+
+    return train_dataset, val_dataset, test_dataset
