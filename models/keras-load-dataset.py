c9377fec3f7d7b9dfabf32131ac4b4b693ad44e4
Charles Pan <cpan22@stanford.edu>
2020-11-14 15:50:26 -0500

    Removed validation dataset from splitGroups, this can be done during Model.fit

--- models/keras-load-dataset.py
+++ models/keras-load-dataset.py
@@ -11,19 +11,13 @@ def loadDataset(img_directory):
 
 def splitGroups(dataset):
     dataset_size = tf.data.experimental.cardinality(dataset)
-
-    train_size = int(0.8 * dataset_size)
-    val_size = int(0.1 * dataset_size)
-    test_size = int(0.1 * dataset_size)
+    train_size = int(0.9 * dataset_size)
 
     dataset = dataset.shuffle()
     train_dataset = dataset.take(train_size)
     test_dataset = dataset.skip(train_size)
-    val_dataset = test_dataset.skip(val_size)
-    test_dataset = test_dataset.take(test_size)
 
     print("Number of training samples: %d" % tf.data.experimental.cardinality(train_dataset))
-    print("Number of validation samples: %d" % tf.data.experimental.cardinality(val_dataset))
     print("Number of test samples: %d" % tf.data.experimental.cardinality(test_dataset))
 
-    return train_dataset, val_dataset, test_dataset
+    return train_dataset, test_dataset
