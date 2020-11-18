a01729d959823614107f73555a782b29a210e397
Charles Pan <cpan22@stanford.edu>
2020-11-17 21:16:28 -0500

    Replaced scores with predictions for ROC AUC

--- models/ResNet-pretrained.py
+++ models/ResNet-pretrained.py
@@ -56,25 +56,27 @@ model = keras.Model(inputs, outputs)
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
     )
 
-predictions = tf.argmax(input=model.predict(test_set), axis=1).numpy()
+scores = model.predict(test_set)
+predictions = tf.argmax(input=scores, axis=1).numpy()
 f = open("pretrained-output.txt", "a")
 f.write("BEFORE TRAINING EVALUATION\n")
 f.write("MODEL EVALUATION (loss, metrics): " + str(model.evaluate(test_set)) + "\n")
 f.write("BALANCED ACCURACY: " + str(metrics.balanced_accuracy_score(labels, predictions)) + "\n")
 f.write("CONFUSION MATRIX: " + str(metrics.confusion_matrix(labels, predictions)) + "\n")
-f.write("ROC AUC SCORE: " + str(metrics.roc_auc_score(labels, predictions)) + "\n")
+f.write("ROC AUC SCORE: " + str(metrics.roc_auc_score(labels, scores)) + "\n")
 
 with strategy.scope():
     epochs = 20
     model.fit(train_set, epochs=epochs, validation_data=val_set)
 
-predictions = tf.argmax(input=model.predict(test_set), axis=1).numpy()
+scores = model.predict(test_set)
+predictions = tf.argmax(input=scores, axis=1).numpy()
 f = open("pretrained-output.txt", "a")
 f.write("AFTER FINE TUNING EVALUATION\n")
 f.write("MODEL EVALUATION (loss, metrics): " + str(model.evaluate(test_set)) + "\n")
 f.write("BALANCED ACCURACY: " + str(metrics.balanced_accuracy_score(labels, predictions)) + "\n")
 f.write("CONFUSION MATRIX: " + str(metrics.confusion_matrix(labels, predictions)) + "\n")
-f.write("ROC AUC SCORE: " + str(metrics.roc_auc_score(labels, predictions)) + "\n")
+f.write("ROC AUC SCORE: " + str(metrics.roc_auc_score(labels, scores)) + "\n")
 
 with strategy.scope():
     # fine tune over the whole model
@@ -90,10 +92,11 @@ with strategy.scope():
     epochs = 10
     model.fit(train_set, epochs=epochs, validation_data=val_set)
 
-predictions = tf.argmax(input=model.predict(test_set), axis=1).numpy()
+scores = model.predict(test_set)
+predictions = tf.argmax(input=scores, axis=1).numpy()
 f = open("pretrained-output.txt", "a")
 f.write("AFTER FULL MODEL TRAINING EVALUATION\n")
 f.write("MODEL EVALUATION (loss, metrics): " + str(model.evaluate(test_set)) + "\n")
 f.write("BALANCED ACCURACY: " + str(metrics.balanced_accuracy_score(labels, predictions)) + "\n")
 f.write("CONFUSION MATRIX: " + str(metrics.confusion_matrix(labels, predictions)) + "\n")
-f.write("ROC AUC SCORE: " + str(metrics.roc_auc_score(labels, predictions)) + "\n")
+f.write("ROC AUC SCORE: " + str(metrics.roc_auc_score(labels, scores)) + "\n")
